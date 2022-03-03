dcbee357af1829102d0d08fdf3286054f19e7735
alejandra fusil <alejandrafusil@gmail.com>
2022-03-03 02:10:15 +0000

    arreglar iconos

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+import App from "./app";
 import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.querySelector("#app")
+);
